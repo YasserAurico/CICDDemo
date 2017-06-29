f0c72a89b7e03b9e3a95a8641de1658915a4c3eb
ymostafa <ymostafa@LATR-YMOSTAFA1>
2017-06-29 10:29:09 -0500

    Initial Commit

---
+++ CICDDemo/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CICDDemo.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [Route("{name?}")]
+        [HttpGet]
+        public IHttpActionResult TestMe(string name = null)
+        {
+            string greeting = System.Configuration.ConfigurationManager.AppSettings["greeting"];
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                greeting += " Your name is: " + name;
+            }
+
+            return Ok(greeting);
+
+        }
+    }
+}
